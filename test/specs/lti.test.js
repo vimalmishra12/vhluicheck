81e154e3696a8bf7c2b58fd520f91029ea4a0536
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-09-15 16:45:39 +0530

    commit updates

--- test/specs/lti.test.js
+++ test/specs/lti.test.js
@@ -7,7 +7,7 @@ describe('VHl Reader', () => {
         testdata = await fs.readFileSync('./testdata/' + argv.env + '.json')
         testjson = JSON.parse(testdata)
         await LoginPage.open(testjson.readerurl);
-        browser.pause(10000)
+        browser.pause(5000)
         await LoginPage.login(argv.userName, argv.password);
         await expect(HeaderPage.headerPage).toBeExisting();
     });
