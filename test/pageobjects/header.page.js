903216218e41f35473dc59c24effe84ee6189559
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-09-15 17:47:07 +0530

    check

--- test/pageobjects/header.page.js
+++ test/pageobjects/header.page.js
@@ -4,6 +4,7 @@ class HeaderPage {
      * define selectors using getter methods
      */
     get headerPage () {
+        browser.pause(5000)
         return $('[data-tid=text-pageTitle]');
     }
 }
